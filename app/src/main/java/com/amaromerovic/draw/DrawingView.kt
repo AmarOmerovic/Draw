6590b34482565affc91d99853b5bf5c116e5705f
amaromerovic <amaromerovic473@gmail.com>
2022-10-19 18:53:24 +0200

    Initial commit

---
+++ app/src/main/java/com/amaromerovic/draw/DrawingView.kt
@@ -0,0 +1,87 @@
+package com.amaromerovic.draw
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+
+class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+    private lateinit var drawPath: CustomPath
+    private lateinit var canvasBitmap: Bitmap
+    private lateinit var drawPaint: Paint
+    private lateinit var canvasPaint: Paint
+    private var brushSize: Float = 0.toFloat()
+    private var color = Color.BLACK
+    private lateinit var canvas: Canvas
+    private val paths = ArrayList<CustomPath>()
+
+    init {
+        setUp()
+    }
+
+    private fun setUp() {
+        drawPaint = Paint()
+        drawPath = CustomPath(color, brushSize)
+        drawPaint.color = color
+        drawPaint.style = Paint.Style.STROKE
+        drawPaint.strokeJoin = Paint.Join.ROUND
+        drawPaint.strokeCap = Paint.Cap.ROUND
+        canvasPaint = Paint(Paint.DITHER_FLAG)
+        brushSize = 20.toFloat()
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(canvasBitmap)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(canvasBitmap, 0f, 0f, canvasPaint)
+
+        for (path in paths) {
+            if (!path.isEmpty) {
+                drawPaint.strokeWidth = path.brushThickness
+                drawPaint.color = path.color
+                canvas.drawPath(path, drawPaint)
+            }
+        }
+
+        if (!drawPath.isEmpty) {
+            drawPaint.strokeWidth = drawPath.brushThickness
+            drawPaint.color = drawPath.color
+            canvas.drawPath(drawPath, drawPaint)
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                drawPath.color = color
+                drawPath.brushThickness = brushSize
+
+                drawPath.reset()
+                drawPath.moveTo(touchX!!, touchY!!)
+            }
+            MotionEvent.ACTION_MOVE -> {
+                drawPath.lineTo(touchX!!, touchY!!)
+            }
+            MotionEvent.ACTION_UP -> {
+                paths.add(drawPath)
+                drawPath = CustomPath(color, brushSize)
+            }
+            else -> return false
+        }
+        invalidate()
+        return true
+    }
+
+    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
+
+    }
+}
